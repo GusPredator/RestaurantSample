19905adc7e62088ba140c4f737ad1f4ec1f02020
Gustavo Sánchez <gustavo.sanchez@korinver.com>
2019-01-12 20:41:07 -0600

    Test

--- resources/js/mainMenu.js
+++
@@ -1,9 +0,0 @@
-const thisWindow = require("electron").remote.getCurrentWindow();
-
-const exitBtn = document.getElementById("exitBtn");
-
-function exit(){
-    thisWindow.close();
-}
-
-exitBtn.addEventListener('click', exit);
